23274a5fb10ab6f16f989f97c302d2f02abbcec4
Nicholas Kell <nick@monkeyknight.com>
2012-05-03 15:07:43 -0500

    Edited uncompress.sh removed all sudo commnads

--- uncompress.sh
+++ uncompress.sh
@@ -1,28 +1,28 @@
 #!/bin/bash
 
-ISO=/home/nick/Desktop/lupu-528.iso
+ISO=old.iso
 UNCOMPRESSED_DIR=uncompressed
 ISO_MOUNT_DIR=iso_mount
 INIT_GZ=initrd.gz
 INIT_TREE=initrd-tree
 
-sudo rm -rf $UNCOMPRESSED_DIR
-sudo mkdir $UNCOMPRESSED_DIR
+rm -rf $UNCOMPRESSED_DIR
+mkdir $UNCOMPRESSED_DIR
 
-sudo mkdir $ISO_MOUNT_DIR
-sudo mount -o loop $ISO $ISO_MOUNT_DIR
+mkdir $ISO_MOUNT_DIR
+mount -o loop $ISO $ISO_MOUNT_DIR
 cd $ISO_MOUNT_DIR
 
-sudo cp * ../$UNCOMPRESSED_DIR/
+cp * ../$UNCOMPRESSED_DIR/
 cd ..
-sudo umount $ISO_MOUNT_DIR
-sudo rm -rf $ISO_MOUNT_DIR
+umount $ISO_MOUNT_DIR
+rm -rf $ISO_MOUNT_DIR
 cd $UNCOMPRESSED_DIR
-sudo unsquashfs lupu_528.sfs
+unsquashfs lupu_528.sfs
 
-sudo mkdir $INIT_TREE
+mkdir $INIT_TREE
 cd $INIT_TREE
-sudo zcat ../$INIT_GZ | sudo cpio -d -i
+zcat ../$INIT_GZ | sudo cpio -d -i
 cd ../..
 
 ls $UNCOMPRESSED_DIR
